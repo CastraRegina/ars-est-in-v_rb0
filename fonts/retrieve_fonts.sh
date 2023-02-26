38acb294f1b0c36edb0dfc02b121e3c8465706b0
Regina Castra <castraregina@gmx.net>
2023-02-26 22:03:01 +0100

    Overwrite *.ttf files when unzipping

--- fonts/retrieve_fonts.sh
+++ fonts/retrieve_fonts.sh
@@ -3,6 +3,7 @@
 
 mkdir zips
 
+wget https://fonts.google.com/download?family=Dancing%20Script   -O zips/Dancing_Script.zip
 wget https://fonts.google.com/download?family=Grandstander       -O zips/Grandstander.zip
 wget https://fonts.google.com/download?family=Noto%20Sans%20Mono -O zips/Noto_Sans_Mono.zip
 wget https://fonts.google.com/download?family=Recursive          -O zips/Recursive.zip
@@ -10,6 +11,6 @@ wget https://fonts.google.com/download?family=Roboto%20Flex      -O zips/Roboto_
 wget https://fonts.google.com/download?family=Roboto%20Mono      -O zips/Roboto_Mono.zip
 
 for i in zips/*.zip ; do
-    unzip "${i}" "*.ttf" -x "static/*" 
+    unzip -o "${i}" "*.ttf" -x "static/*" 
 done
 
