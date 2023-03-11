b35cf8ff066cade6efa6d45bc17fcd32194e3551
Regina Castra <castraregina@gmx.net>
2023-03-11 22:02:58 +0100

    Add example how to handle variable font instancer

--- src/examples/fonts/print_fonttools_glyph_metrics.py
+++ src/examples/fonts/print_fonttools_glyph_metrics.py
@@ -4,6 +4,7 @@ ascent, descent, line gap, and units per em of a given character in a font.
 """
 
 from fontTools.ttLib import TTFont
+from fontTools.varLib import instancer
 from fontTools.pens.boundsPen import BoundsPen
 
 
@@ -44,10 +45,15 @@ def print_glyph_metrics(font: TTFont, char: str) -> None:
 
 if __name__ == '__main__':
 
-    FONT_FILENAME = "fonts/NotoSansMono-VariableFont_wdth,wght.ttf"
+    # FONT_FILENAME = "fonts/Grandstander-VariableFont_wght.ttf"
+    # FONT_FILENAME = "fonts/NotoSansMono-VariableFont_wdth,wght.ttf"
+    # FONT_FILENAME = "fonts/Recursive-VariableFont_CASL,CRSV,MONO,slnt,wght.ttf"
+    FONT_FILENAME = "fonts/RobotoFlex-VariableFont_GRAD,XTRA,YOPQ,YTAS,YTDE,YTFI,YTLC,YTUC,opsz,slnt,wdth,wght.ttf"
+    # FONT_FILENAME = "fonts/RobotoMono-VariableFont_wght.ttf"
 
     # Load the font file
     ttfont = TTFont(FONT_FILENAME)
+    font_family = ttfont['name'].getDebugName(1)
 
     # Print the metrics for some characters:
     print_glyph_metrics(ttfont, 'ä')
@@ -62,3 +68,28 @@ if __name__ == '__main__':
     print_glyph_metrics(ttfont, '\\')
     print_glyph_metrics(ttfont, '-')
     print_glyph_metrics(ttfont, '_')
+
+    print("--------------------------------------")
+    print(f'Available axes of {font_family}:')
+    for axis in ttfont['fvar'].axes:
+        print(f'  - {axis.axisTag}: ' +
+              f'{axis.minValue:7.1f} to {axis.maxValue:7.1f}, ' +
+              f'default: {axis.defaultValue:7.1f} ')
+
+    print("--------------------------------------")
+    print("Standard default (wght=400):")
+    print_glyph_metrics(ttfont, 'Ä')
+
+    print("Example wght=400:")
+    varfont = instancer.instantiateVariableFont(ttfont, {"wght": 400})
+    print_glyph_metrics(varfont, 'Ä')
+
+    print("Example wght=200:")
+    varfont = instancer.instantiateVariableFont(ttfont, {"wght": 200})
+    print_glyph_metrics(varfont, 'Ä')
+
+    print("Example wght=700:")
+    varfont = instancer.instantiateVariableFont(ttfont, {"wght": 700})
+    print_glyph_metrics(varfont, 'Ä')
+
+    print("--------------------------------------")
