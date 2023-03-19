bbed4b46b7f3505c79b9abe82102e1de75e8d4c7
Regina Castra <castraregina@gmx.net>
2023-03-19 11:56:19 +0100

    Separate font info from gyph info

--- src/examples/fonts/print_fonttools_glyph_metrics.py
+++ src/examples/fonts/print_fonttools_glyph_metrics.py
@@ -8,9 +8,41 @@ from fontTools.varLib import instancer
 from fontTools.pens.boundsPen import BoundsPen
 
 
+def print_font_metrics(font: TTFont) -> None:
+    """
+    Prints the ascent, descent, line gap, units per em, ...
+    of a given font.
+
+    Args:
+        font (TTFont): The font object.
+    """
+    # Extract the font information from the different tables
+    ascender_hhea = font['hhea'].ascender
+    ascender_os2 = font['OS/2'].sTypoAscender
+    descender_hhea = font['hhea'].descender
+    descender_os2 = font['OS/2'].sTypoDescender
+    line_gap_hhea = font['hhea'].lineGap
+    line_gap_os2 = font['OS/2'].sTypoLineGap
+    units_per_em = font['head'].unitsPerEm
+    x_height = font["OS/2"].sxHeight
+    cap_height = font["OS/2"].sCapHeight
+
+    # Print the font metrics
+    print("Font metrics:")
+    print("    ascender_hhea  :", ascender_hhea)
+    print("    ascender_os2   :", ascender_os2)
+    print("    descender_hhea :", descender_hhea)
+    print("    descender_os2  :", descender_os2)
+    print("    line_gap_hhea  :", line_gap_hhea)
+    print("    line_gap_os2   :", line_gap_os2)
+    print("    units_per_em   :", units_per_em)
+    print("    x_height       :", x_height)
+    print("    cap_height     :", cap_height)
+
+
 def print_glyph_metrics(font: TTFont, char: str) -> None:
     """
-    Prints the bounding box, width, ascent, descent, line gap, and units per em
+    Prints the glyph name, bounding box and width
     of a given character in a font.
 
     Args:
@@ -25,22 +57,11 @@ def print_glyph_metrics(font: TTFont, char: str) -> None:
     bounds_pen = BoundsPen(font.getGlyphSet())
     glyph.draw(bounds_pen)
 
-    # Calculate the width, ascent, descent, line gap, and units per em
-    width = glyph.width
-    ascent = font['hhea'].ascent
-    descent = font['hhea'].descent
-    line_gap = font['hhea'].lineGap
-    units_per_em = font['head'].unitsPerEm
-
     # Print the glyph metrics
     print(f'Glyph metrics for character "{char}":')
-    print("  glyph name:", glyph_name)
-    print("  Bounding box:", bounds_pen.bounds)
-    print("  Width:", width)
-    print("  Ascent:", ascent)
-    print("  Descent:", descent)
-    print("  Line gap:", line_gap)
-    print("  Units per em:", units_per_em)
+    print("    glyph name     :", glyph_name)
+    print("    bounding box   :", bounds_pen.bounds)
+    print("    width          :", glyph.width)
 
 
 if __name__ == '__main__':
@@ -52,11 +73,19 @@ if __name__ == '__main__':
                     "YOPQ,YTAS,YTDE,YTFI,YTLC,YTUC,opsz,slnt,wdth,wght.ttf"
     # FONT_FILENAME = "fonts/RobotoMono-VariableFont_wght.ttf"
 
-    # Load the font file
     ttfont = TTFont(FONT_FILENAME)
     font_family = ttfont['name'].getDebugName(1)
 
-    # Print the metrics for some characters:
+    print("--------------------------------------")
+    print(f'Available axes of {font_family}:')
+    for axis in ttfont['fvar'].axes:
+        print(f'  - {axis.axisTag}: ' +
+              f'{axis.minValue:7.1f} to {axis.maxValue:7.1f}, ' +
+              f'default: {axis.defaultValue:7.1f} ')
+
+    print_font_metrics(ttfont)
+
+    print("--------------------------------------")
     print_glyph_metrics(ttfont, 'ä')
     print_glyph_metrics(ttfont, 'ö')
     print_glyph_metrics(ttfont, 'ü')
@@ -71,26 +100,26 @@ if __name__ == '__main__':
     print_glyph_metrics(ttfont, '_')
 
     print("--------------------------------------")
-    print(f'Available axes of {font_family}:')
-    for axis in ttfont['fvar'].axes:
-        print(f'  - {axis.axisTag}: ' +
-              f'{axis.minValue:7.1f} to {axis.maxValue:7.1f}, ' +
-              f'default: {axis.defaultValue:7.1f} ')
-
-    print("--------------------------------------")
     print("Standard default (wght=400):")
     print_glyph_metrics(ttfont, 'Ä')
+    print_font_metrics(ttfont)
 
+    print()
     print("Example wght=400:")
     varfont = instancer.instantiateVariableFont(ttfont, {"wght": 400})
     print_glyph_metrics(varfont, 'Ä')
+    print_font_metrics(ttfont)
 
+    print()
     print("Example wght=200:")
     varfont = instancer.instantiateVariableFont(ttfont, {"wght": 200})
     print_glyph_metrics(varfont, 'Ä')
+    print_font_metrics(ttfont)
 
+    print()
     print("Example wght=700:")
     varfont = instancer.instantiateVariableFont(ttfont, {"wght": 700})
     print_glyph_metrics(varfont, 'Ä')
+    print_font_metrics(ttfont)
 
     print("--------------------------------------")
