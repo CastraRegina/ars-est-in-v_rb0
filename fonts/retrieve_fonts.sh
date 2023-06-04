60a483784877aa30838280e50d20701d9de45edb
Regina Castra <castraregina@gmx.net>
2023-06-04 17:45:58 +0200

    retrieve font Cantarell

--- fonts/retrieve_fonts.sh
+++ fonts/retrieve_fonts.sh
@@ -9,6 +9,7 @@ wget https://fonts.google.com/download?family=Noto%20Sans%20Mono -O zips/Noto_Sa
 wget https://fonts.google.com/download?family=Recursive          -O zips/Recursive.zip
 wget https://fonts.google.com/download?family=Roboto%20Flex      -O zips/Roboto_Flex.zip
 wget https://fonts.google.com/download?family=Roboto%20Mono      -O zips/Roboto_Mono.zip
+wget https://fonts.google.com/download?family=Cantarell          -O zips/Cantarell.zip
 
 for i in zips/*.zip ; do
     unzip -o "${i}" "*.ttf" -x "static/*" 
