74170fe03ebd7e9cb83644fc4a8d6cee0c8bde6b
fk <castraregina@gmx.net>
2023-12-20 21:34:01 +0100

    updates due formatter black and lint-errors

--- src/examples/png/test_get_section.py
+++ src/examples/png/test_get_section.py
@@ -1,6 +1,4 @@
-# from PIL import Image
 import PIL.Image
-# import Image from PIL
 import numpy as np
 
 
@@ -14,33 +12,29 @@ class AVImage:
                 self.image = self.image.convert(convert)
             (self.width, self.height) = self.image.size
 
-    def section(x: float, y: float,
-                width: float, height: float) -> PIL.Image:
+    def section(self, x: float, y: float, width: float, height: float) -> PIL.Image:
         # relative to width 0..1
         if not self.image:
             return None
         # TODO: x,y,width,height min(0) max(1)
         # TODO: round()
 
-    def avg_value(x: float, y: float,
-                  width: float, height: float) -> float:
+    def avg_value(self, x: float, y: float, width: float, height: float) -> float:
         pass
         # TODO
 
-    def max_value(x: float, y: float,
-                  width: float, height: float) -> float:
+    def max_value(self, x: float, y: float, width: float, height: float) -> float:
         pass
         # TODO
 
-    def min_value(x: float, y: float,
-                  width: float, height: float) -> float:
+    def min_value(self, x: float, y: float, width: float, height: float) -> float:
         pass
         # TODO
 
 
 def extract_section(image_path, x, y, width, height):
     # Open the image file
-    image = PIL.Image.open(image_path).convert('L')  # Convert to grayscale
+    image = PIL.Image.open(image_path).convert("L")  # Convert to grayscale
 
     # Extract the section based on coordinates
     section = image.crop((x, y, x + width, y + height))
@@ -67,12 +61,12 @@ def save_section_as_image(section, output_path):
 
 
 # Example usage
-image_path = 'data/output/example/png/test_board_10grays.png'
+image_path = "data/output/example/png/test_board_10grays.png"
 x = 0  # X-coordinate of the top-left corner of the section
-y = 0   # Y-coordinate of the top-left corner of the section
-width = 700   # Width of the section
+y = 0  # Y-coordinate of the top-left corner of the section
+width = 700  # Width of the section
 height = 700  # Height of the section
-output_path = 'section_output.png'
+output_path = "section_output.png"
 
 # Extract the section from the image
 section = extract_section(image_path, x, y, width, height)
@@ -81,8 +75,7 @@ section = extract_section(image_path, x, y, width, height)
 save_section_as_image(section, output_path)
 
 # Calculate the average gray value of the section
-average_gray_value = calculate_average_gray_value(
-    image_path, x, y, width, height)
+average_gray_value = calculate_average_gray_value(image_path, x, y, width, height)
 print(f"The average gray value of the section is: {average_gray_value}")
 print(f"The section has been saved as: {output_path}")
 
