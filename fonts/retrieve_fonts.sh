d66ba71d67784db51ab36a73f0953757a1c7dfb8
fk <castraregina@gmx.net>
2024-06-09 10:46:30 +0200

    update: download does not work anymore

--- fonts/retrieve_fonts.sh
+++ fonts/retrieve_fonts.sh
@@ -3,13 +3,17 @@
 
 mkdir zips
 
-wget https://fonts.google.com/download?family=Dancing%20Script   -O zips/Dancing_Script.zip
-wget https://fonts.google.com/download?family=Grandstander       -O zips/Grandstander.zip
-wget https://fonts.google.com/download?family=Noto%20Sans%20Mono -O zips/Noto_Sans_Mono.zip
-wget https://fonts.google.com/download?family=Recursive          -O zips/Recursive.zip
-wget https://fonts.google.com/download?family=Roboto%20Flex      -O zips/Roboto_Flex.zip
-wget https://fonts.google.com/download?family=Roboto%20Mono      -O zips/Roboto_Mono.zip
-wget https://fonts.google.com/download?family=Cantarell          -O zips/Cantarell.zip
+# download by using wget does not work anymore:
+#wget https://fonts.google.com/download?family=Dancing%20Script   -O zips/Dancing_Script.zip
+#wget https://fonts.google.com/download?family=Grandstander       -O zips/Grandstander.zip
+#wget https://fonts.google.com/download?family=Noto%20Sans%20Mono -O zips/Noto_Sans_Mono.zip
+#wget https://fonts.google.com/download?family=Recursive          -O zips/Recursive.zip
+#wget https://fonts.google.com/download?family=Roboto%20Flex      -O zips/Roboto_Flex.zip
+#wget https://fonts.google.com/download?family=Roboto%20Mono      -O zips/Roboto_Mono.zip
+#wget https://fonts.google.com/download?family=Cantarell          -O zips/Cantarell.zip
+#wget https://fonts.google.com/download?family=Petrona            -O zips/Petrona.zip
+#wget https://fonts.google.com/download?family=Caveat             -O zips/Caveat.zip
+
 
 for i in zips/*.zip ; do
     unzip -o "${i}" "*.ttf" -x "static/*" 
