6b42cad60e926329cb7b21a699a9e7fb9e4b23ea
fk <castraregina@gmx.net>
2024-11-30 18:41:57 +0100

    add folder output/example/svg/ave

--- data/create_output_folders.sh
+++ data/create_output_folders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FOLDERS="output/example output/example/png output/example/png/checkerboard output/example/svg"
+FOLDERS="output/example output/example/png output/example/png/checkerboard output/example/svg output/example/svg/ave"
 
 for folder in ${FOLDERS} ; do
   echo mkdir -p ${folder}
