2efca0907d750bd988fc41151cebd61f33cd32b1
fk <castraregina@gmx.net>
2024-12-24 19:34:05 +0100

    add transform functionalities

--- src/ave/geom.py
+++ src/ave/geom.py
@@ -2,9 +2,43 @@
 
 from __future__ import annotations
 
-from typing import Tuple
+from typing import Sequence, Tuple, Union
 
 
+class GeomHelper:
+    """Class to provide various static methods related to geometry handling."""
+
+    @staticmethod
+    def transform_point(
+        affine_trafo: Sequence[Union[int, float]], point: Sequence[Union[int, float]]
+    ) -> Tuple[float, float]:
+        """
+        Perform an affine transformation on the given 2D point.
+
+        The given _affine_trafo_ is a list of 6 floats, performing an affine transformation.
+        The transformation is defined as:
+            | x' | = | a00 a01 b0 |   | x |
+            | y' | = | a10 a11 b1 | * | y |
+            | 1  | = |  0   0  1  |   | 1 |
+        with
+            affine_trafo = [a00, a01, a10, a11, b0, b1]
+        See also shapely - Affine Transformations
+
+        Args:
+            affine_trafo (Tuple/List[float]): Affine transformation - [a00, a01, a10, a11, b0, b1]
+            point (Tuple/List[float]): 2D point - (x, y)
+
+        Returns:
+            Tuple[float, float]: the transformed point
+        """
+        x_new = float(affine_trafo[0] * point[0] + affine_trafo[1] * point[1] + affine_trafo[4])
+        y_new = float(affine_trafo[2] * point[0] + affine_trafo[3] * point[1] + affine_trafo[5])
+        return (x_new, y_new)
+
+
+# =============================================================================
+# Box
+# =============================================================================
 class AvBox:
     """
     Represents a rectangular box with coordinates and dimensions.
@@ -79,6 +113,54 @@ class AvBox:
 
         return self.width * self.height
 
+    @property
+    def centroid(self) -> Tuple[float, float]:
+        """
+        The centroid of the box.
+
+        Returns:
+            Tuple[float, float]: The coordinates of the centroid as (x, y)
+        """
+        return (self._xmin + self._xmax) / 2, (self._ymin + self._ymax) / 2
+
+    def __str__(self):
+        """Returns a string representation of the AvBox instance."""
+
+        return (
+            f"AvBox(xmin={self._xmin}, ymin={self._ymin}, "
+            f"      xmax={self._xmax}, ymax={self._ymax}, "
+            f"      width={self.width}, height={self.height})"
+        )
+
+    def transform_affine(self, affine_trafo: Sequence[Union[int, float]]) -> AvBox:
+        """
+        Transform the AvBox using the given affine transformation [a00, a01, a10, a11, b0, b1].
+
+        Args:
+            affine_trafo (List[float]): Affine transformation [a00, a01, a10, a11, b0, b1]
+
+        Returns:
+            AvBox: The transformed box
+        """
+        (xmin, ymin, xmax, ymax) = self.extent
+        (x0, y0) = GeomHelper.transform_point(affine_trafo, (xmin, ymin))
+        (x1, y1) = GeomHelper.transform_point(affine_trafo, (xmax, ymax))
+        return AvBox(xmin=x0, ymin=y0, xmax=x1, ymax=y1)
+
+    def transform_scale_translate(self, scale_factor: float, translate_x: float, translate_y: float) -> AvBox:
+        """
+        Transform the AvBox using the given scale and translation.
+
+        Args:
+            scale_factor (float): The scale factor.
+            translate_x (float): The translation in x-direction.
+            translate_y (float): The translation in y-direction.
+
+        Returns:
+            AvBox: The transformed box
+        """
+        return self.transform_affine((scale_factor, 0, 0, scale_factor, translate_x, translate_y))
+
 
 def main():
     """Main"""
@@ -90,7 +172,17 @@ def main():
     print(f"Area  : {my_box.area}")
     print(f"xmin  : {my_box.xmin}, ymin: {my_box.ymin}, xmax: {my_box.xmax}, ymax: {my_box.ymax}")
 
-    # scale = font_size / units_per_em
+    my_box = my_box.transform_scale_translate(1, 10, 20)
+    print(f"Width : {my_box.width}")
+    print(f"Height: {my_box.height}")
+    print(f"Area  : {my_box.area}")
+    print(f"xmin  : {my_box.xmin}, ymin: {my_box.ymin}, xmax: {my_box.xmax}, ymax: {my_box.ymax}")
+
+    my_box = my_box.transform_affine((2, 0, 0, 2, 0, 0))
+    print(f"Width : {my_box.width}")
+    print(f"Height: {my_box.height}")
+    print(f"Area  : {my_box.area}")
+    print(f"xmin  : {my_box.xmin}, ymin: {my_box.ymin}, xmax: {my_box.xmax}, ymax: {my_box.ymax}")
 
 
 if __name__ == "__main__":
