15dfdb88663288b1ad4a4877353ffeb29d0e3297
fk <castraregina@gmx.net>
2025-11-22 16:28:04 +0100

    add infos how to reinstall python modules to update-script

--- update_python_modules.sh
+++ update_python_modules.sh
@@ -14,12 +14,22 @@ python3 --version
 
 #exit
 echo "---------------------------------"
-#python3 -m pip install --upgrade pip setuptools wheel ; python3 -m pip freeze | cut -d'=' -f1 | xargs -n1 python3 -m pip install -U ; python3 -m pip freeze > requirements.txt
+# python3 -m pip install --upgrade pip setuptools wheel ; python3 -m pip freeze | cut -d'=' -f1 | xargs -n1 python3 -m pip install -U ; python3 -m pip freeze > requirements.txt
 echo "---------------------------------"
 python3 -m pip install --upgrade pip setuptools wheel
 echo "---------------------------------"
 python3 -m pip freeze | cut -d'=' -f1 | xargs -n1 python3 -m pip install -U
 echo "---------------------------------"
 python3 -m pip freeze > requirements.txt
+
+
+
+
+# howto setup venv and install python modules:
+# /usr/bin/python3 -m venv venv
+# . venv/bin/activate
+# python3 -m pip install --upgrade pip setuptools wheel
+# python3 -m pip install -r requirements.txt
+# python3 -m pip install --upgrade pip setuptools wheel ; python3 -m pip freeze | cut -d'=' -f1 | xargs -n1 python3 -m pip install -U ; python3 -m pip freeze > requirements.txt
 
 
