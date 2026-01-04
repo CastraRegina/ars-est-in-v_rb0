012f6c6a600bc8326a2819c1b62cb8885b63b040
fk <castraregina@gmx.net>
2026-01-04 11:27:48 +0100

    Add support for multiple test board layouts in grayscale image generator

--- src/examples/png/png_draw_test_board_10grays.py
+++ src/examples/png/png_draw_test_board_10grays.py
@@ -1,52 +1,70 @@
-"""Creates an image with 10 different "gray" squares from white to black.
-"""
-from PIL import Image
+"""Creates an image with 10 different "gray" squares from white to black."""
+
 import numpy as np
-
-
-OUTPUT_FILENAME = "data/output/example/png/test_board_10grays.png"
+from PIL import Image
 
 WIDTH_OF_SQUARE = 100  # width [pixel] of one square
-# BOARD_LAYOUT: 0=black, 9=white
-BOARD_LAYOUT = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 9, 9, 9, 9, 9, 0],
-    [0, 9, 1, 5, 2, 9, 0],
-    [0, 9, 8, 0, 6, 9, 0],
-    [0, 9, 4, 7, 3, 9, 0],
-    [0, 9, 9, 9, 9, 9, 0],
-    [0, 0, 0, 0, 0, 0, 0],
+
+OUTPUT_FILENAMES = [
+    "data/output/example/png/test_board_07x07_10grays.png",
+    "data/output/example/png/test_board_09x09_10grays.png",
+]
+
+# BOARD_LAYOUT: 0=black(0), 9=white(255)
+BOARD_LAYOUTS = [
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 9, 9, 9, 9, 9, 0],
+        [0, 9, 1, 5, 2, 9, 0],
+        [0, 9, 8, 0, 6, 9, 0],
+        [0, 9, 4, 7, 3, 9, 0],
+        [0, 9, 9, 9, 9, 9, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [9, 9, 9, 9, 9, 9, 9, 9, 9],
+        [9, 0, 0, 0, 0, 0, 0, 0, 9],
+        [9, 0, 9, 9, 9, 9, 9, 0, 9],
+        [9, 0, 9, 1, 5, 2, 9, 0, 9],
+        [9, 0, 9, 8, 0, 6, 9, 0, 9],
+        [9, 0, 9, 4, 7, 3, 9, 0, 9],
+        [9, 0, 9, 9, 9, 9, 9, 0, 9],
+        [9, 0, 0, 0, 0, 0, 0, 0, 9],
+        [9, 9, 9, 9, 9, 9, 9, 9, 9],
+    ],
 ]
 COLOR_SCALE = 255 / 9  # scale factor to scale max color value of board to white
 
-# Retrieve height and width of the layout
-BOARD_HEIGHT = len(BOARD_LAYOUT)
-BOARD_WIDTH = len(BOARD_LAYOUT[0])
-
 
 def main():
-    """Creates an image with 10 different "gray" squares from white to black."""
-    # Create a blank image with a white background
-    board = np.zeros((BOARD_HEIGHT, BOARD_WIDTH, 3))
+    """Creates images with 10 different 'gray' squares from white to black."""
+    # Iterate over all layouts and create corresponding images
+    for board_layout, output_filename in zip(BOARD_LAYOUTS, OUTPUT_FILENAMES):
+        # Retrieve height and width of the current layout
+        board_height = len(board_layout)
+        board_width = len(board_layout[0])
 
-    # Set the squares of the board to certain gray values
-    for i in range(BOARD_HEIGHT):
-        for j in range(BOARD_WIDTH):
-            gray = BOARD_LAYOUT[i][j] * COLOR_SCALE
-            board[i, j] = [gray, gray, gray]
+        # Create a blank image with a white background
+        board = np.zeros((board_height, board_width, 3))
 
-    # Create a PIL image from the numpy array
-    image = Image.fromarray(np.uint8(board))
+        # Set the squares of the board to certain gray values
+        for i in range(board_height):
+            for j in range(board_width):
+                gray = board_layout[i][j] * COLOR_SCALE
+                board[i, j] = [gray, gray, gray]
 
-    # Resize the image to X*X pixels for each square
-    image = image.resize(
-        (BOARD_WIDTH * WIDTH_OF_SQUARE, BOARD_HEIGHT * WIDTH_OF_SQUARE),
-        Image.Resampling.NEAREST,
-    )
+        # Create a PIL image from the numpy array
+        image = Image.fromarray(np.uint8(board))
 
-    # Save the image as a png file
-    image.save(OUTPUT_FILENAME)
-    print(f"{OUTPUT_FILENAME} saved.")
+        # Resize the image to X*X pixels for each square
+        image = image.resize(
+            (board_width * WIDTH_OF_SQUARE, board_height * WIDTH_OF_SQUARE),
+            Image.Resampling.NEAREST,
+        )
+
+        # Save the image as a png file
+        image.save(output_filename)
+        print(f"{output_filename} saved.")
 
 
 if __name__ == "__main__":
